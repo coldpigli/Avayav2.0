6961e92150d476048fa375188591fbdb1ea17f04
coldpigli <piyushdas98@gmail.com>
2022-03-16 00:09:40 +0530

    Feat: Added Basic Rotuing Functionalities

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Route, Link,Routes } from "react-router-dom";
+import { Cart, ErrorPage, Home, Wishlist } from "./routes";
+
+
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/Cart">Cart</Link>
+        <Link to="/Wishlist">Wishlist</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Cart" element={<Cart />} />
+        <Route path="/Wishlist" element={<Wishlist />} />
+        <Route path="*" element={<ErrorPage/>} />
+      </Routes>
     </div>
   );
 }
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import Home from "./Home";
+import Wishlist from "./Wishlist";
+import Cart from "./Cart";
+import ErrorPage from "./ErrorPage";
+
+
+export {
+    Home,
+    Wishlist,
+    Cart,
+    ErrorPage
+}
