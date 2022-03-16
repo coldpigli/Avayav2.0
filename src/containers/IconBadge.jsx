e12b189a41019c62b7fae11cbf1bf173fa57f8e7
coldpigli <piyushdas98@gmail.com>
2022-03-16 13:01:08 +0530

    Feat: Added Icon Badge Component

---
+++ src/containers/IconBadge.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const IconBadge = ({nextPath, iconName, count }) => {
+  return (
+    <div className="badge">
+      <Link to={nextPath}>
+        <span className="material-icons md-24">{iconName}</span>
+      </Link>
+      <div className="badge-value top-right">{count}</div>
+    </div>
+  );
+}
+
+export default IconBadge
